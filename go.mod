c879ede5a17799c3b494a89097149712c8655d63
squeaky1273 <pjriddell22@gmail.com>
2020-07-05 20:35:58 -0700

    [getting close]

--- go.mod
+++ go.mod
@@ -11,7 +11,10 @@ require (
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/jinzhu/gorm v1.9.14 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	google.golang.org/appengine v1.6.5 // indirect
--- scrape.go
+++ scrape.go
@@ -10,25 +10,32 @@ import (
 )
 
 type Post struct {
-	Title          string  `json:"title"`
-	NumofUpvotes   string  `json:"upvotes"`
-	// User   	       string  `json:"user"`
+	Title          string
+	NumofUpvotes   string
+	// User   	       string
 }
 
 type Posts struct {
-	Posts []Post `json:"posts"`
+	Posts []Post
 }
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
-func main() {
-	posts := scrapeReddit()
-	serializePosts(posts)
+
+func jsonReddit(filename string, e Posts) {
+	jsonPost, err := json.Marshal(e)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := ioutil.WriteFile("output.json", jsonPost, os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func scrapeReddit() {
+func main() {
 	c := colly.NewCollector()
-
 	var posts Posts
 
 	c.OnHTML(".rpBJOHq2PR60pnwJlUyP0", func(wrapper *colly.HTMLElement) {
@@ -44,13 +51,16 @@ func scrapeReddit() {
 			if k < 10 {
 				posts.Posts[k].NumofUpvotes = upvotes.Text
 			}
-
 		})
-		// // On every a element which has author/user attribute call callback
-		// c.ForEach("#t3_hi3oht > div._1poyrkZ7g36PawDueRza-J._11R7M_VOgKO1RJyRSRErT3 > div.BiNC74axuTz66dlnEgicY > div.cZPZhMe-UCZ8htPodMyJ5 > div._3AStxql1mQsrZuUIFP9xSg.nU4Je7n-eSXStTBAPMYt8 > div:nth-child(4) > a", func(e *colly.HTMLElement) {
-		// 	fmt.Printf("User: %s\n", e.Text)
-		// })
+
+		j, _ := json.Marshal(posts)
+		_ = ioutil.WriteFile("output.json", j, os.ModePerm)
+
+		for _, post := range posts.Posts {
+			jsonReddit("output.json", post)
+		}
 	})
+
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
@@ -59,14 +69,45 @@ func scrapeReddit() {
 	c.Visit("https://www.reddit.com/")
 }
 
-func serializePosts(posts Post) {
-	j, err := json.Marshal(posts)
+// func main() {
+// 	c := colly.NewCollector()
 
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	var posts []Posts
 
-	if err := ioutil.WriteFile("output.json", j, os.ModePerm); err != nil {
-		log.Fatal(err)
-	}
-}
+// 	c.OnHTML(".rpBJOHq2PR60pnwJlUyP0", func(wrapper *colly.HTMLElement) {
+// 		// On every a element which has title attribute call callback
+	
+// 		title := e.ChildText("div.y8HYJ-y_lTUHkQIc1mdCq._2INHSNB8V5eaWp4P0rY_mE")
+// 		upvotes := e.ChildText("div._23h0-EcaBUorIHC-JZyh6J > div > div")
+
+// 		posts = append(posts, clothing)
+// 		// // On every a element which has author/user attribute call callback
+// 		// c.ForEach("#t3_hi3oht > div._1poyrkZ7g36PawDueRza-J._11R7M_VOgKO1RJyRSRErT3 > div.BiNC74axuTz66dlnEgicY > div.cZPZhMe-UCZ8htPodMyJ5 > div._3AStxql1mQsrZuUIFP9xSg.nU4Je7n-eSXStTBAPMYt8 > div:nth-child(4) > a", func(e *colly.HTMLElement) {
+// 		// 	fmt.Printf("User: %s\n", e.Text)
+// 		// })
+// 	})
+// 	// Before making a request print "Visiting ..."
+// 	c.OnRequest(func(r *colly.Request) {
+// 		fmt.Println("Visiting", r.URL.String())
+// 	})
+
+// 	c.Visit("https://www.reddit.com/")
+
+// 	enc := json.NewEncoder(os.Stdout)
+//     enc.SetIndent("", "  ")
+
+//     // Dump json to the standard output
+//     enc.Encode(clothes)
+// }
+
+// func serializePosts(posts Post) {
+// 	j, err := json.Marshal(posts)
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	if err := ioutil.WriteFile("output.json", j, os.ModePerm); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
